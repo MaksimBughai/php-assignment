fef11f30b4082a2f05bb0551e08f2d0504265574
Maksim <maximus@load.me>
2023-01-11 10:15:01 +0100

    realized noop calculator + worte unit tests for this statistic

--- app/module/Statistics/src/Calculator/NoopCalculator.php
+++ app/module/Statistics/src/Calculator/NoopCalculator.php
@@ -9,12 +9,60 @@ use Statistics\Dto\StatisticsTo;
 
 class NoopCalculator extends AbstractCalculator
 {
+
+    protected const UNITS = 'posts';
+
+    /**
+     * array of count posts per author in selected period
+     * @var array
+     */
+    private $countPostsPerAuthor = [];
+
+    /**
+     * array which has info about author_id => author_name
+     * @var array
+     */
+    private $authorsName = [];
+
     /**
      * @inheritDoc
      */
     protected function doAccumulate(SocialPostTo $postTo): void
     {
-        // Noops!
+        $authorId = $postTo->getAuthorId();
+        $authorName = $postTo->getAuthorName();
+
+        if( !isset( $this->authorsName[ $authorId ] ) ){
+            $this->authorsName[ $authorId ] = $authorName;
+        }
+
+        $this->countPostsPerAuthor[ $authorId ]  =  ($this->countPostsPerAuthor[ $authorId ] ?? 0) + 1;
+    }
+
+    /**
+     * function for getting months from selected period
+     *
+     * we need to see all months from period because we may have case when in months we don`t any have posts
+     */
+    protected function getUsedMonths(): array
+    {
+        $usedMonths = [];
+
+        $startDate = $this->parameters->getStartDate();
+        $endDate = $this->parameters->getEndDate();
+
+        $inPeriod = true;
+        while( $inPeriod ){
+            $month = $startDate->format('Y-m');
+            $usedMonths[ $month ] = $month;
+
+            $startDate->modify('+1 month');
+            if( $startDate->getTimestamp() > $endDate->getTimestamp() ){
+                $inPeriod = false;
+            }
+        }
+
+        return $usedMonths;
     }
 
     /**
@@ -22,6 +70,29 @@ class NoopCalculator extends AbstractCalculator
      */
     protected function doCalculate(): StatisticsTo
     {
-        return new StatisticsTo();
+        $stats = new StatisticsTo();
+        $usedMonths = $this->getUsedMonths();
+
+        // if we don`t have posts in selected period
+        if( !count( $this->countPostsPerAuthor )
+            || !count( $usedMonths ) ){
+            return $stats;
+        }
+
+        $countMonthInPeriod = max( count( $usedMonths ), 1 );
+
+        foreach ($this->countPostsPerAuthor as $authorId => $totalPosts) {
+            $authorName = $this->authorsName[ $authorId ] ?? $authorId;
+            $averagePosts = ceil( $totalPosts/$countMonthInPeriod );
+
+            $child = (new StatisticsTo())
+                ->setName( $authorName )
+                ->setSplitPeriod( $authorName )
+                ->setValue( $averagePosts )
+                ->setUnits(self::UNITS );
+            $stats->addChild($child);
+        }
+
+        return $stats;
     }
 }
